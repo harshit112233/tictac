5275de1543a17d8109277a5f4d8c94dbd07c8e1b
Harshit Sukhija <61389611+harshit112233@users.noreply.github.com>
2023-11-01 16:55:33 +0530

    Add files via upload

---
+++ temp4.py
@@ -0,0 +1,14 @@
+pricelist = [1000, 2000, 3000, 4000, 7000, 10000]
+cash = int(input("Enter put price : "))
+price = float(input("Enter share rate : "))
+if cash==3000:
+    for i in range(4):
+        price = price-price*2/100
+        exit = price + price*2/100
+        print(round(price, 2), pricelist[i+2], "cp+2")
+if cash==1000:
+    for i in range(5):
+        price = price-price*2/100
+        exit = price + price*2/100
+        print(round(price, 2), pricelist[i+1], "cp+2")
+        
