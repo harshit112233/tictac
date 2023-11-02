ed5d68e376b4ec75102669af87c7537ccbded1fe
Harshit Sukhija <61389611+harshit112233@users.noreply.github.com>
2023-11-02 09:47:43 +0530

    Update temp4.py

--- temp4.py
+++ temp4.py
@@ -1,6 +1,7 @@
 pricelist = [1000, 2000, 3000, 4000, 7000, 10000]
 cash = int(input("Enter put price : "))
 price = float(input("Enter share rate : "))
+price1 = float(input("Enter future current rate : "))
 if cash==3000:
     for i in range(4):
         price = price-price*2/100
@@ -11,4 +12,4 @@ if cash==1000:
         price = price-price*2/100
         exit = price + price*2/100
         print(round(price, 2), pricelist[i+1], "cp+2")
-        
+print(round(price1*2/100, 2))
